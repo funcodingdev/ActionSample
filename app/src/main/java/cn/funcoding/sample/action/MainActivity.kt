58eda509338101a4f8758cc885f2560d1c8e204c
fangju <fangju@wepie.com>
2021-05-10 20:36:28 +0800

    edit MainActivity.kt

--- app/src/main/java/cn/funcoding/sample/action/MainActivity.kt
+++ app/src/main/java/cn/funcoding/sample/action/MainActivity.kt
@@ -2,10 +2,17 @@ package cn.funcoding.sample.action
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(Companion.TAG, "onCreate: ")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
